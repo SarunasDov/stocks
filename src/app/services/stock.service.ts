76a83a42084ba892baa3300d66cdbee0182cb001
Sarunas Dovydaitis <82829974+SarunasDov@users.noreply.github.com>
2021-11-07 14:44:13 +0200

    stocks

---
+++ src/app/services/stock.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Stock } from '../models/stock';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getStocks(): Observable<Stock[]>{
+    return this.httpClient.get<Stock[]>('http://localhost:3000/api/stock');
+  }
+}
